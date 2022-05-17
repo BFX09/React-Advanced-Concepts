b6b0de9e436e954f4caa2569e6b57426e6d94260
BFX09 <bardia.azari@gmail.com>
2022-05-17 16:56:50 +0430

    Initial Commit.

---
+++ src/hoc/Movie.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import withTooltip from "./withTooltip";
+
+class Movie extends Component {
+  state = {};
+  render() {
+    return (
+    <div>
+     Movie
+     {this.props.showTooltip && <div>Some Tooltip</div>}
+    </div>);
+  }
+}
+
+export default withTooltip(Movie);
